27008a48bb3180a0b9ae91828a4073ae8066800d
Artur Drzyzga <artek.drzyzga@gmail.com>
2021-06-02 09:06:13 +0200

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'tweets/index'
+  get 'tweets/new'
+  get 'tweets/show'
+  get 'tweets/edit'
+
+
+  root to: "tweets#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
